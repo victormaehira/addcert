c15f41d997063d6ed9681e58d190f2ce8108ad01
Victor Yuji Maehira <victor.maehira@certis>
2026-04-15 15:56:31 -0300

    adicionado senha na chave privada

--- CertTokenExtension/TokenSession.swift
+++ CertTokenExtension/TokenSession.swift
@@ -2,8 +2,16 @@ import CryptoTokenKit
 import Security
 
 class TokenSession: TKTokenSession, TKTokenSessionDelegate {
+    
+    static let privateKeyPassword = "12345678"
+    
     static let privateKey = "MIIEowIBAAKCAQEAt/KNX0Kq4LGc/AIVsAisYA2BOImtP7HTL6oB6k9BvJ/PE20YGdpDK3OXvcm1Q/k5b5kmnjdPBAMeTXQe5hiSvxBr0RpWykSB9EqIvRMr+k72/UgxqHdDopCu2LUudK85FkMdlFbO7UMDl9d/YZS1xPHX4i9Cid/+NobCmvy9WbKCQU1x5XjEnDhcntRXTYMnIT+SAib1hKPwM89AsWfQiE30WKU+y1Xxa0D1kxpnSCH+xzrjtWJU2eNoMLN4NOQecU36vCrUh80iLEZLHH5mmVT9NKP6NPe3/oAFOYwwv7fQAQ3x0E/T85yEFqEd9efw/hFTLB1T28RgeG4NaQQbLwIDAQABAoIBAC9yGJThlUGvjlZSE1X8Zrm/wfzbRhyPuXEp4KSXHNWSQs837Gd+rKSghBsn0+FcfzwRvKxCh9b5Fu/Ta8TdwbwWeDjPGmPBl+Ny9iIOt+EwTPS3kldpq8BaoT60PO9L4uWjGhYQ7f60slCP/QMmYFwUJSLqHbeCVweparifSBfCSzc8dH6dC6uOTizSF+6V7nkNU8lfJ9A52yYxK7zOv7AH4iD9hTpHmjyYsT5GDzgXLvZOpB9/WblL8mcy0WQDbSTfTqHSNlHenGXDFvCYv7AcbSoHIdiNHbXX1RjqNLEssdjZhQln2RJ2fFyVfKev2jJeQWuwrIcprQk0+slEo0ECgYEA56yHkRpIYujDQrIYMklAn5CRjasWbdBS6aCvv591SoYAPodN1HoPk6JjkfyJY4T5fBogUWYBxSE9YNT5Das6tUz33HdcbkCxRoC2JjD4HNRhjOU1iORLx+VdIiIWcunEzv8/MD/u6i+pBCofQ6+Bv1bpLBNmV6RUTlhralmZYvMCgYEAy0MeoMcL4SWOJ3JdVGC7upYyAJqf1pXhoiL9Ty9ZreMsXCZlvRG3dNyRlC/WeW5+yXf0Aev6Sc4g0tnFbqy5Q7NaTjqd0Hf7DSf/LAMninP1DhPCGhane0KbwUc0mLYQhRpZ36/tDTqRgNi7I9vV/cPdGb10GsABop53IJpj3dUCgYAVvYUDQtokHf+k1J2cqm8cCi7+gl2adIAzWFblvor9MVH4jC3rkIDBs+1wF6i05BedY75ApTfpTdM6sQGmHLlnpg9kavcLiZqZKR7uuo2t9ugolqHNdM7/tTBmMZi1s+Y6Ho3Jc1ZyN4K+100TxvaABCHhdviVpOAccgOyeTIBrQKBgQDK/T/UHpQdh+zcNhlAj85K+336HnEr0sjfrAO/FbGAt5Nwf6Qw2kWVPkVgcRnGcXPK7bFQTgLJvEIJcBP8gCAQnUe9Qkqii3+7Vss9f/T4Du+W3GKGvUMLFK0Jq3u6WnBLDNLeUgnxoSD4RPk3SW7+m6Dt+Ma/hCrd5VVgyO6c9QKBgFlV2AGeNqpLcL+OwkgMVh04hpLAeKJyaLWhCF2ZTRFtu2+6QyAoRQc8Ey4qkh0Zjho8pH4VL6o9py8yRyJkm6eXB33VOtOOeulyFkPTIzC52dz13sTjM4+cO1gQf7BB9utrIIctvAPDZ7CMhWZ0DNcyseUWleB/63w+qg9yoGim"
-
+    
+    //private key Victor Yuji Maehira corporativo sem senha
+    /*
+    static let privateKey = "MIIEowIBAAKCAQEAt/KNX0Kq4LGc/AIVsAisYA2BOImtP7HTL6oB6k9BvJ/PE20YGdpDK3OXvcm1Q/k5b5kmnjdPBAMeTXQe5hiSvxBr0RpWykSB9EqIvRMr+k72/UgxqHdDopCu2LUudK85FkMdlFbO7UMDl9d/YZS1xPHX4i9Cid/+NobCmvy9WbKCQU1x5XjEnDhcntRXTYMnIT+SAib1hKPwM89AsWfQiE30WKU+y1Xxa0D1kxpnSCH+xzrjtWJU2eNoMLN4NOQecU36vCrUh80iLEZLHH5mmVT9NKP6NPe3/oAFOYwwv7fQAQ3x0E/T85yEFqEd9efw/hFTLB1T28RgeG4NaQQbLwIDAQABAoIBAC9yGJThlUGvjlZSE1X8Zrm/wfzbRhyPuXEp4KSXHNWSQs837Gd+rKSghBsn0+FcfzwRvKxCh9b5Fu/Ta8TdwbwWeDjPGmPBl+Ny9iIOt+EwTPS3kldpq8BaoT60PO9L4uWjGhYQ7f60slCP/QMmYFwUJSLqHbeCVweparifSBfCSzc8dH6dC6uOTizSF+6V7nkNU8lfJ9A52yYxK7zOv7AH4iD9hTpHmjyYsT5GDzgXLvZOpB9/WblL8mcy0WQDbSTfTqHSNlHenGXDFvCYv7AcbSoHIdiNHbXX1RjqNLEssdjZhQln2RJ2fFyVfKev2jJeQWuwrIcprQk0+slEo0ECgYEA56yHkRpIYujDQrIYMklAn5CRjasWbdBS6aCvv591SoYAPodN1HoPk6JjkfyJY4T5fBogUWYBxSE9YNT5Das6tUz33HdcbkCxRoC2JjD4HNRhjOU1iORLx+VdIiIWcunEzv8/MD/u6i+pBCofQ6+Bv1bpLBNmV6RUTlhralmZYvMCgYEAy0MeoMcL4SWOJ3JdVGC7upYyAJqf1pXhoiL9Ty9ZreMsXCZlvRG3dNyRlC/WeW5+yXf0Aev6Sc4g0tnFbqy5Q7NaTjqd0Hf7DSf/LAMninP1DhPCGhane0KbwUc0mLYQhRpZ36/tDTqRgNi7I9vV/cPdGb10GsABop53IJpj3dUCgYAVvYUDQtokHf+k1J2cqm8cCi7+gl2adIAzWFblvor9MVH4jC3rkIDBs+1wF6i05BedY75ApTfpTdM6sQGmHLlnpg9kavcLiZqZKR7uuo2t9ugolqHNdM7/tTBmMZi1s+Y6Ho3Jc1ZyN4K+100TxvaABCHhdviVpOAccgOyeTIBrQKBgQDK/T/UHpQdh+zcNhlAj85K+336HnEr0sjfrAO/FbGAt5Nwf6Qw2kWVPkVgcRnGcXPK7bFQTgLJvEIJcBP8gCAQnUe9Qkqii3+7Vss9f/T4Du+W3GKGvUMLFK0Jq3u6WnBLDNLeUgnxoSD4RPk3SW7+m6Dt+Ma/hCrd5VVgyO6c9QKBgFlV2AGeNqpLcL+OwkgMVh04hpLAeKJyaLWhCF2ZTRFtu2+6QyAoRQc8Ey4qkh0Zjho8pH4VL6o9py8yRyJkm6eXB33VOtOOeulyFkPTIzC52dz13sTjM4+cO1gQf7BB9utrIIctvAPDZ7CMhWZ0DNcyseUWleB/63w+qg9yoGim"
+     */
+     
     private static var cachedSecKey: SecKey?
 
     private static func getPrivateSecKey() throws -> SecKey {
@@ -19,20 +27,34 @@ class TokenSession: TKTokenSession, TKTokenSessionDelegate {
             ])
         }
 
-        let attributes: [String: Any] = [
-            kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
-            kSecAttrKeyClass as String: kSecAttrKeyClassPrivate,
-            kSecAttrKeySizeInBits as String: 2048
-        ]
+        var format = SecExternalFormat.formatUnknown
+        var itemType = SecExternalItemType.itemTypePrivateKey
 
-        var error: Unmanaged<CFError>?
-        guard let secKey = SecKeyCreateWithData(keyData as CFData, attributes as CFDictionary, &error) else {
-            let desc = error?.takeRetainedValue().localizedDescription ?? "unknown error"
-            throw NSError(domain: "CertTokenExtension", code: -2, userInfo: [
-                NSLocalizedDescriptionKey: "Failed to create SecKey: \(desc)"
+        var keyParams = SecItemImportExportKeyParameters()
+        keyParams.version = UInt32(SEC_KEY_IMPORT_EXPORT_PARAMS_VERSION)
+        keyParams.passphrase = Unmanaged.passRetained(privateKeyPassword as CFTypeRef)
+
+        var items: CFArray?
+        let status = SecItemImport(
+            keyData as CFData,
+            nil,
+            &format,
+            &itemType,
+            SecItemImportExportFlags(),
+            &keyParams,
+            nil,
+            &items
+        )
+
+        guard status == errSecSuccess,
+              let importedItems = items,
+              CFArrayGetCount(importedItems) > 0 else {
+            throw NSError(domain: "CertTokenExtension", code: Int(status), userInfo: [
+                NSLocalizedDescriptionKey: "Failed to import encrypted private key (status: \(status))"
             ])
         }
 
+        let secKey = unsafeBitCast(CFArrayGetValueAtIndex(importedItems, 0), to: SecKey.self)
         cachedSecKey = secKey
         NSLog("CertTokenExtension: RSA private key loaded successfully")
         return secKey
